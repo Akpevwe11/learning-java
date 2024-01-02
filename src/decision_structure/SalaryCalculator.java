191bad2038d84bde294d6631e0b56b30b9a82ece
Praise Essi <pessi@parkway.ng>
2024-01-02 01:25:38 +0100

    decision making

---
+++ src/decision_structure/SalaryCalculator.java
@@ -0,0 +1,12 @@
+package src.decision_structure;
+
+public class SalaryCalculator {
+    public static void main(String[] args) {
+
+        double salary = 1000;
+        double bonus = 200;
+        int quota = 10;
+
+        System.out.println("How many sales did the employee get this week?");
+    }
+}
